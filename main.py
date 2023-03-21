cf8f06f3abf45405c4b46cc2b2d0b7d5edac25fb
13120801478 <maidou199612@126.com>
2023-03-21 22:22:29 +0800

    。。

--- main.py
+++ main.py
@@ -2,11 +2,22 @@
 import os
 from configs.dir_path import configs_path
 from utils.yaml_handle import read_yaml
-from libs.login_token import Login
+import requests
+import json
 
 # login = Login()
 # res = login.get_access_token(os.path.join(configs_path, 'access_token.yaml'))
 
 
-login = Login()
-login.get_access_token(os.path.join(configs_path, 'access_token.yaml'))
+# url = 'http://127.0.0.1:5000/post_method_one'
+# payload = {'first': 2, 'second': 3}
+# headers = {"Content-Type": "application/json"}
+# res = requests.post(url, data=json.dumps(payload), headers=headers).json()
+# print(res)
+
+
+# url = 'http://127.0.0.1:5000/post_method_two'
+# payload = {'first': 2, 'second': 3}
+# headers = {"Content-Type": "application/x-www-form-urlencoded"}
+# res = requests.post(url, data=payload, headers=headers).json()
+# print(res)
--- utils/mock_handle.py
+++ utils/mock_handle.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
-from flask import Flask
+from flask import Flask, request, Response, jsonify
+import requests
 
 app = Flask(__name__)
 
@@ -8,4 +9,25 @@ app = Flask(__name__)
 def index():
     return 'Hello world'
 
-app.run()
+
+@app.route('/post_method_one', methods=['POST'])
+def post_method_one():
+    try:
+        res = {'sum': request.json['first'] + request.json['second'], 'errorcode': 'success'}
+        return jsonify(res)
+    except Exception as e:
+        res = {'errorcode': 'error'}
+        return jsonify(res)
+
+
+@app.route('/post_method_two', methods=['POST'])
+def post_method_two():
+    try:
+        res = {'sum': int(request.form['first']) + int(request.form['second']), 'errorcode': 'success'}
+        return jsonify(res)
+    except Exception as e:
+        res = {'errorcode': 'error'}
+        return jsonify(res)
+
+
+app.run(debug=True)
